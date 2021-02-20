58b65912581b3f7410ab1d3a71cb46c4dfb4d77d
Rahat <djrayhan8@gmail.com>
2021-02-20 22:49:08 +0600

    Completed the UI of Dashboard

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem() {
+export default function EventListItem({ event }) {
     return (
         <Segment.Group>
             <Segment>
@@ -11,32 +11,41 @@ export default function EventListItem() {
                         <Item.Image
                             size="tiny"
                             circular
-                            src="assets/user.png"
+                            src={event.hostPhotoURL}
                         />
                         <Item.Content>
-                            <Item.Header content="Event Title" />
-                            <Item.Description>Hosted By Rahat</Item.Description>
+                            <Item.Header content={event.title} />
+                            <Item.Description>
+                                Hosted By {event.hostedBy}
+                            </Item.Description>
                         </Item.Content>
                     </Item>
                 </Item.Group>
             </Segment>
             <Segment>
                 <span>
-                    <Icon name="clock" /> Date
-                    <Icon name="marker" /> Venue
+                    <Icon name="clock outline" /> {event.date}
+                    <Icon name="map marker alternate" /> {event.venue}
                 </span>
             </Segment>
             <Segment secondary>
                 <List horizontal>
-                    <EventListAttendee />
-                    <EventListAttendee />
-                    <EventListAttendee />
-                    <EventListAttendee />
+                    {event.attendees.map(attendee => (
+                        <EventListAttendee
+                            key={attendee.id}
+                            attendee={attendee}
+                        />
+                    ))}
                 </List>
             </Segment>
             <Segment clearing>
-                <div>Description of Events</div>
-                <Button color="teal" floated="right" content="View" />
+                <div>{event.description}</div>
+                <Button animated floated="right" color="teal">
+                    <Button.Content visible>View</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name="arrow right" />
+                    </Button.Content>
+                </Button>
             </Segment>
         </Segment.Group>
     );
---
+++ src/features/events/eventForm/EventForm.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
+
+export default function EventForm() {
+    return (
+        <Segment clearing>
+            <Header content="Create New Event" />
+            <Form>
+                <Form.Field>
+                    <input type="text" placeholder="Event Title" />
+                </Form.Field>
+
+                <Form.Field>
+                    <input type="text" placeholder="Category" />
+                </Form.Field>
+
+                <Form.Field>
+                    <input type="text" placeholder="Description" />
+                </Form.Field>
+
+                <Form.Field>
+                    <input type="text" placeholder="City" />
+                </Form.Field>
+
+                <Form.Field>
+                    <input type="text" placeholder="Venue" />
+                </Form.Field>
+
+                <Form.Field>
+                    <input type="date" placeholder="Date" />
+                </Form.Field>
+                <Button animated type="submit" positive floated="right">
+                    <Button.Content visible>Submit</Button.Content>
+                    <Button.Content hidden>Submit</Button.Content>
+                </Button>
+                <Button
+                    animated="vertical"
+                    negative
+                    type="submit"
+                    floated="right"
+                >
+                    <Button.Content visible>Cancel</Button.Content>
+                    <Button.Content hidden>Cancel</Button.Content>
+                </Button>
+            </Form>
+        </Segment>
+    );
+}
